50f4184c0594dbc4986e9750d1b9e7a78d1b5b12
SvetlanaMikhmel <129420501+SvetlanaMikhmel@users.noreply.github.com>
2023-04-11 16:24:24 +0300

    Ex9

---
+++ src/main/java/ru/teachmeskills/homework3/Exercise9.java
@@ -0,0 +1,12 @@
+package ru.teachmeskills.homework3;
+
+public class Exercise9 {
+    // Необходимо, чтоб программа выводила на экран вот такую
+    //последовательность:
+    //7 14 21 28 35 42 49 56 63 70 77 84 91 98.
+    public static void main(String[] args) {
+        int i;
+        for (i = 7; i < 100; i+=7)
+            System.out.print(i + " ");
+    }
+}
