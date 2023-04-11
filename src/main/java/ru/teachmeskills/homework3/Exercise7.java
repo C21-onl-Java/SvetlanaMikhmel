5d52615be015d5cc59bcef697e61885abc11a403
SvetlanaMikhmel <129420501+SvetlanaMikhmel@users.noreply.github.com>
2023-04-11 15:46:15 +0300

    Ex7

---
+++ src/main/java/ru/teachmeskills/homework3/Exercise7.java
@@ -0,0 +1,11 @@
+package ru.teachmeskills.homework3;
+
+public class Exercise7 {
+    //Необходимо вывести на экран числа от 5 до 1.
+    //При решении используйте операцию декремента (--).
+    public static void main(String[] args) {
+        int i;
+        for (i = 5; i>=1; i--)
+            System.out.print(i + " ");
+    }
+}
