96239b257f4e37bdf9a4a8a8998dca34257db496
SvetlanaMikhmel <129420501+SvetlanaMikhmel@users.noreply.github.com>
2023-04-11 16:31:15 +0300

    Ex9

--- src/main/java/ru/teachmeskills/homework3/Exercise9.java
+++ src/main/java/ru/teachmeskills/homework3/Exercise9.java
@@ -5,8 +5,11 @@ public class Exercise9 {
     //последовательность:
     //7 14 21 28 35 42 49 56 63 70 77 84 91 98.
     public static void main(String[] args) {
-        int i;
-        for (i = 7; i < 100; i+=7)
+        int i = 7;
+        while (i < 100) {
             System.out.print(i + " ");
+            i += 7;
+        }
+
     }
 }
